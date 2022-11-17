ec25ca077e7031230a1550c6cba443afeca22dab
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-17 12:01:31 +0300

    Task 51

--- Task051/Program.cs
+++ Task051/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+
+int[,] FillMatrixRnd(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd  = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = rnd.Next(min, max);
+            }
+    } 
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5} |");
+            else Console.Write($"{matrix[i,j], 5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+int SumDiagonalElem(int [,] matrix)
+{
+    int sum = 0;
+    int diag = 0;
+    if (matrix.GetLength(0) > matrix.GetLength(1))
+        diag = matrix.GetLength(1);
+    else
+        diag = matrix.GetLength(0);
+    for (int i = 0; i < diag; i++)
+        sum += matrix[i,i];
+
+  return sum;
+}
+
+Console.WriteLine("Введите количество строк:");
+int matrixRows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int matrixColums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное ограничение массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное ограничение массива:");
+int b = Convert.ToInt32(Console.ReadLine())+1;
+int [,] myMatrix = FillMatrixRnd(matrixRows, matrixColums, a, b);
+PrintMatrix(myMatrix);
+Console.WriteLine(" ");
+int newMyMatrix = SumDiagonalElem(myMatrix);
+Console.WriteLine(newMyMatrix);
