55b6390bc5eea471d99efe24bc97e932e672dd0d
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-17 11:30:44 +0300

    Task 49

--- Task049/Program.cs
+++ Task049/Program.cs
@@ -1,4 +1,55 @@
 ﻿// Задайте двумерный массив. Найдите элементы, 
 //у которых оба индекса чётные, и замените эти элементы на их квадраты.
 
-Console.WriteLine("Hello, World!");
+int[,] FillMatrixRnd(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd  = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = rnd.Next(min, max);
+            }
+    } 
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5} |");
+            else Console.Write($"{matrix[i,j], 5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+int [,] RepleceElemForSque(int [,] repmatrix)
+{
+    for (int i = 0; i < repmatrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < repmatrix.GetLength(1); j+=2)
+            {
+                //if (i % 2==0 && j % 2 ==0);
+                repmatrix[i,j] = (repmatrix[i,j]) * (repmatrix[i,j]);
+            }
+    } 
+    return repmatrix;
+}
+
+Console.WriteLine("Введите количество строк:");
+int matrixRows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int matrixColums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное ограничение массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное ограничение массива:");
+int b = Convert.ToInt32(Console.ReadLine())+1;
+int [,] myMatrix = FillMatrixRnd(matrixRows, matrixColums, a, b);
+PrintMatrix(myMatrix);
+Console.WriteLine(" ");
+int [,] newMyMatrix = RepleceElemForSque(myMatrix);
+PrintMatrix(newMyMatrix);
